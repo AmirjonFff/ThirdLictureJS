12fd7dcabbd77f27f604d687a677d98733b62572
AmirjonFff <fayzmuhammadamirjoni@gmail.com>
2023-04-18 00:41:32 +0500

    HomeWork licture 3 dey7_JS

---
+++ README.md
@@ -0,0 +1,113 @@
+![The San Juan Mountains are beautiful!](/img/imgasosi.png "San Juan Mountains") <br>
+### What is a Method in java script?
+**Метод** - это блок кода, который запускается только при вызова. Методы исползуется для выполнения определенних дейсвий, и они также известные как функции. <br>
+
+## String in java script
+####  1. charAt()
+Получает элемент по индексу. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(2).png "San Juan Mountains") <br>
+####  2. concat()
+Объединяет несколько текстов. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(3).png "San Juan Mountains") <br>
+####  3. replace()
+Метод replace() возвращает новую строку с замененными значениями. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(4).png "San Juan Mountains") <br>
+####  4. replaceAll()
+Метод replaceAll() возвращает новую строку, в которой все совпадения строку заменены на
+замена. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(5).png "San Juan Mountains") <br>
+####  5. split()
+Возврашает страка на массив <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(6).png "San Juan Mountains") <br>
+####  6. substring(start,end)
+Метод substring() извлекает символы от start до end (эксклюзивно). <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(7).png "San Juan Mountains") <br>
+####  7.  slice(start, end)
+Метод substring() извлекает символы от start до end (эксклюзивно). И может иметь отрицательное значение <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(8).png "San Juan Mountains") <br>
+####  8.  toLowerCase()
+Делает все буквы строчными. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(9).png "San Juan Mountains") <br>
+####  9. toUpperCase()
+Делает все буквы с болшими. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(10).png "San Juan Mountains") <br>
+####  10.   includes()
+Метод include() возвращает true, если строка содержит указанную строку.
+В противном случае он возвращает ложь.. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(11).png "San Juan Mountains") <br>
+####  11.  toString()
+Возврашает все типи на строку <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(12).png "San Juan Mountains") <br>
+####  12.  indexOf()
+Метод indexOf() возвращает позицию первого вхождения значения в строку. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(13).png "San Juan Mountains") <br>
+####  13.   repeat()
+Метод repeat() создает новую строку, повторяя данную строку заданное количество раз.
+раз и возвращает его. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(14).png "San Juan Mountains") <br>
+####  14.  at()
+Получает элемент по индексу. И может иметь отрицательное значение <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/img%20(1).png "San Juan Mountains") <br>
+
+
+## Number in java script
+
+#### 1.  Math.round()
+Функция Math.round() возвращает число, округленное до ближайшего целого числа. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(1).png.png "San Juan Mountains") <br>
+#### 2.  Math.ceil()
+Метод ceil() округляет десятичное число до следующего наибольшего целого числа и возвращает его. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(2).png "San Juan Mountains") <br>
+#### 3.   Math.floor()
+Функция Math.floor() округляет число до следующего наименьшего целого числа. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(3).png "San Juan Mountains") <br>
+#### 4.   Math.max() 
+Возвращает максималние числа. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(4).png "San Juan Mountains") <br>
+#### 5.  Math.min()
+Возвращает минималние числа. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(5).png "San Juan Mountains") <br>
+#### 6.   Math.pow() 
+Метод pow() вычисляет степень числа. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(6).png "San Juan Mountains") <br>
+#### 7.  Math.sqrt()
+Метод sqrt() вычисляет корень числа. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(7).png "San Juan Mountains") <br>
+#### 8.   Math.abs()
+делает число положителние. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(8).png "San Juan Mountains") <br>
+#### 9.  Math.random()
+Функция Math.random() возвращает псевдослучайное число с плавающей точкой от 0 (включительно)
+и 1 (исключительно). <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(9).png "San Juan Mountains") <br>
+#### 10.  isNaN()
+Функция isNaN() проверяет, является ли значение NaN (Not-a-Number) или нет. <br>
+Например: <br>
+![The San Juan Mountains are beautiful!](/img/imgnum%20(10).png "San Juan Mountains") <br>
+
+
+
+
---
+++ main.js
@@ -0,0 +1,268 @@
+// let arr = [1,'dsd',10,'15']
+//Methot of String
+
+// // charAt() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.charAt(0));
+// console.log(str2.charAt(11));
+// console.log(str2.charAt(5));
+
+// // concat() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.concat("i ", "Fayzmuhammad"));
+// console.log(str1.concat(str1, str2));
+// console.log(str1.concat(" ", str1));
+
+// // replace() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.replace("o", "a"));
+// console.log(str1.replace("am", "your"));
+// console.log(str2.replace("SoftClub", "TNU"));
+
+// //replaceAll() method of String
+
+// let str1 = "Hello, I am Amirjon am";
+// let str2 = "I study in SoftClub";
+// console.log(str1.replaceAll("o", "a"));
+// console.log(str1.replaceAll("am", "your"));
+// console.log(str2.replaceAll("SoftClub", "TNU"));
+
+// //replaceAll() method of String
+
+// let str1 = "Hello, I am Amirjon am";
+// let str2 = "I study in SoftClub";
+// console.log(str1.split(""));
+// console.log(str1.split(" ", 3));
+// console.log(str2.split("o"));
+
+// //replaceAll() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.substring(4));
+// console.log(str1.substring(4, 8));
+// console.log(str2.substring(0, 15));
+
+// //replaceAll() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.slice(4));
+// console.log(str1.slice(-8, -4));
+// console.log(str2.slice(-15));
+
+// //toLowerCase() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// let str3 = "AAA BBB CCC";
+// console.log(str1.toLowerCase());
+// console.log(str2.toLowerCase());
+// console.log(str3.toLowerCase());
+
+// //toUpperCase() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// let str3 = "aaa bbb ccc";
+// console.log(str1.toUpperCase());
+// console.log(str2.toUpperCase());
+// console.log(str3.toUpperCase());
+
+// //includes() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.includes("am"));
+// console.log(str2.includes("Am"));
+// console.log(str2.includes("I", 2));
+
+// //toString() method of String
+
+// let str1 = [1, 2, "Amir",];
+// let str2 = "I study in SoftClub";
+// let str3 = 10;
+// console.log(str1.toString());
+// console.log(str2.toString());
+// console.log(str3.toString());
+// console.log(typeof(str3.toString()));
+
+// //indexOf() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.indexOf("Am"));
+// console.log(str2.indexOf("in"));
+// console.log(str2.indexOf(" "));
+
+// //repeat() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.repeat(1));
+// console.log(str2.repeat(2));
+// console.log(str2.repeat(3));
+
+// //at() method of String
+
+// let str1 = "Hello, I am Amirjon";
+// let str2 = "I study in SoftClub";
+// console.log(str1.at(3));
+// console.log(str2.at(-2));
+// console.log(str2.at(-1));
+
+
+// //Method of Number
+
+// //round() method of Number
+
+// let num1 = 2.1
+// let num2 = 2.5
+// let num3 = 2.8
+// console.log(Math.round(num1));
+// console.log(Math.round(num2));
+// console.log(Math.round(num3));
+
+// //ceil() method of Number
+
+// let num1 = 2.1
+// let num2 = 2.5
+// let num3 = 2.8
+// console.log(Math.ceil(num1));
+// console.log(Math.ceil(num2));
+// console.log(Math.ceil(num3));
+
+// //floor() method of Number
+
+// let num1 = 2.1
+// let num2 = 2.5
+// let num3 = 2.8
+// console.log(Math.floor(num1));
+// console.log(Math.floor(num2));
+// console.log(Math.floor(num3));
+
+// //max() method of Number
+
+// let num1 = 2.1
+// let num2 = 2.5
+// let num3 = 2.8
+// console.log(Math.max(num1));
+// console.log(Math.max(num2, num1, num3));
+// console.log(Math.max(num3, num3));
+
+// //min() method of Number
+
+// let num1 = 2.1
+// let num2 = 2.5
+// let num3 = 2.8
+// console.log(Math.min(num1));
+// console.log(Math.min(num2, num1, num3));
+// console.log(Math.min(num3, num3));
+
+// //pow() method of Number
+
+// let num1 = 2.1
+// let num2 = 2.5
+// let num3 = 2.8
+// console.log(Math.pow(2, 10));
+// console.log(Math.pow(num2, num1));
+// console.log(Math.pow(num3, num3));
+
+// //sqrt() method of Number
+
+// let num1 = 2.1
+// let num2 = 2.5
+// let num3 = 2.8
+// console.log(Math.sqrt(49));
+// console.log(Math.sqrt(num2));
+// console.log(Math.sqrt(num3));
+
+// //abs() method of Number
+
+// let num1 = 2.1
+// let num2 = -2.5
+// let num3 = -2.8
+// console.log(Math.abs(49));
+// console.log(Math.abs(num2));
+// console.log(Math.abs(num3));
+
+// //random() method of Number
+
+// let num1 = Math.random();
+// let num2 = Math.random()*100; 
+// console.log(num1);
+// console.log(num2);
+
+// for (let i = 0; i < 5; i++) {
+//     let result= Math.random()*10;
+//     console.log(Math.floor(result));
+    
+// }
+
+// //isNaN() method of Number
+
+// let num1 = "Amirjon"
+// let num2 = 2 * num1;
+// let num3 = "50";
+// console.log(isNaN(num1));
+// console.log(isNaN(num2));
+// console.log(isNaN(num3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1
+
+// function givSam(n) {
+//     let str = "SamSing"
+//     return str.concat(" ", n)
+// }
+
+// console.log(givSam("Amir"));
+
+//2
+
+// function givSam(n) {
+//     let str = "SamSing"
+//     return str.concat(" ")
+// }
+
+// console.log(givSam("Amir"));
+
